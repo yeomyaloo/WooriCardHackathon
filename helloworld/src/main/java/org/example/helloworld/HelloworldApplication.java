24cbbf14ac869d927e07ad8c3b7131b7c668b232
yeomyaloo <yeom456@gmail.com>
2024-04-19 17:11:31 +0900

    ㅋㅋ

---
+++ helloworld/src/main/java/org/example/helloworld/HelloworldApplication.java
@@ -0,0 +1,21 @@
+package org.example.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HelloworldApplication {
+
+	@GetMapping("/")
+	public String hello(){
+		return "hello world";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldApplication.class, args);
+	}
+
+}
